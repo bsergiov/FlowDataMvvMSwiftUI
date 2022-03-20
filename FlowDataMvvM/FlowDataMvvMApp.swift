88fe867d91838f1118510744d9e77216a9ebf215
bsergiov <81264661+bsergiov@users.noreply.github.com>
2022-03-20 11:42:19 +0200

    Initial Commit

---
+++ FlowDataMvvM/FlowDataMvvMApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowDataMvvMApp.swift
+//  FlowDataMvvM
+//
+//  Created by BSergio on 20.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FlowDataMvvMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
